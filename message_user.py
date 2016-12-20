35ca2bf7e688753ef60c10cec3375de0f890d133
Eirik Nydal <nydal91@users.noreply.github.com>
2016-12-20 20:15:47 +0100

    Update message_user.py

--- message_user.py
+++ message_user.py
@@ -8,8 +8,8 @@ data = file.read(11)
 print(data)
 
 def load_twilio_config():
-    twilio_account_sid = os.environ.get('ACde1fe2a0de9f60505f00368c41d3d3f1')
-    twilio_auth_token = os.environ.get('d05165fead2edd7c996dd877a96fdb7a')
+    twilio_account_sid = os.environ.get('codeconsoole')
+    twilio_auth_token = os.environ.get('codeconsoole')
     twilio_number = os.environ.get('(951) 821-4625')
     agent_phone_number = os.environ.get('+4794215146')
     return (twilio_number, twilio_account_sid, twilio_auth_token, agent_phone_number)
@@ -22,7 +22,7 @@ def message():
 
 def send_message(msg):
     twilio_phone_number, account_sid, auth_token, agent_phone_number = load_twilio_config()
-    client = TwilioRestClient(account='ACde1fe2a0de9f60505f00368c41d3d3f1', token='d05165fead2edd7c996dd877a96fdb7a')
+    client = TwilioRestClient(account='codeconsoole', token='codeconsoole')
     client.messages.create(to=data,
     from_="(951) 821-4625",
     body=msg)
